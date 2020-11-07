da5ec5b1b85ca2d0d49aa6f44b37c2bbc4799d70
luvhrr <70418587+luvhrr@users.noreply.github.com>
2020-11-07 10:25:13 -0500

    Add files via upload

---
+++ rick-et-morty/README.md
@@ -0,0 +1,13 @@
+# rick-et-morty
+je vos drops le rick es morty unobfusque
+
+-Pour la config.js il faut ouvrir le fichier "config.js"
+
+-ensuite mettre son token en laissant les guillemets de même pour le prefixe
+
+-ouvrez le cmd dans le dossier puis faites 
+`npm i discord.js@11.6.3`
+
+-et il vous reste à lancer le fichier start.bats
+
+et voila vous avez le selfbot rick et morty
---
+++ rick-et-morty/config.js
@@ -0,0 +1,26 @@
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Vous devez Remplir les champs ci-dessous en laissant les guillemets
+*/
+
+       const token = ""; ///tu met ton token ici
+       const prefix = "z"  ///tu met ton prefix ici 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {//NE PAS TOUCHER
+    token,        //NE PAS TOUCHER
+    prefix        //NE PAS TOUCHER
+}                 //NE PAS TOUCHER
